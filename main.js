74f6121837a87b4515be334d99777b0b714e03ae
raizen92 <shin25306@gmail.com>
2014-04-17 10:44:09 -0400

    banner js changes

--- main.js
+++ main.js
@@ -7,3 +7,4 @@ var months = [ "January", "February", "March", "April", "May", "June",
 			 	"July", "August", "September", "October", "November", "December" ],
 	lastMod = new Date(document.lastModified);
 $('.content').append('<div class="pull-right"><i>Eric W '+months[lastMod.getMonth()]+', '+lastMod.getFullYear()+'</i></div>');
+
