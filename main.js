c2880df41ec4af0fe8b587d87bb94c773c66d997
raizen92 <shin25306@gmail.com>
2014-04-17 12:00:06 -0400

    changes to js to detect small screens

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ var months = [ "January", "February", "March", "April", "May", "June",
 	lastMod = new Date(document.lastModified);
 $('.content').append('<div class="pull-right"><i>Eric W '+months[lastMod.getMonth()]+', '+lastMod.getFullYear()+'</i></div>');
 
+function isMobile() {
+   if(window.innerWidth <= 800 && window.innerHeight <= 600) {
+     return true;
+   } else {
+     return false;
+   }
+}
+
+$( document ).ready(function() {
+	if(isMobile()) {
+		$('body').css('width','100%');
+	}
+});
+
