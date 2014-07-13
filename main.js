9b6a8345eece21091906ed543fa37d288c9b3053
Eric Wang <shin25306@gmail.com>
2014-07-13 04:17:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $("#banner").load("banner.html",
 var months = [ "January", "February", "March", "April", "May", "June", 
 			 	"July", "August", "September", "October", "November", "December" ],
 	lastMod = new Date(document.lastModified);
-$('.content').append('<div class="pull-right footer"><i>Eric W '+months[lastMod.getMonth()]+', '+lastMod.getFullYear()+'</i></div>');
+$('.content').append('<div class="pull-right"><i>Eric W '+months[lastMod.getMonth()]+', '+lastMod.getFullYear()+'</i></div>');
 
 function isMobile() {
    if(window.innerWidth <= 800 && window.innerHeight <= 600) {
